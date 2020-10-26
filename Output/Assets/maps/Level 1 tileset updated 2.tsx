c1aecad8a54f966454908726276a5815188f3604
Weedzard <59049803+TitoLuce@users.noreply.github.com>
2020-10-26 11:09:07 +0100

    Embedded tileset with map

--- Output/Assets/maps/Level 1 tileset updated 2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Level 1 tileset updated 2" tilewidth="16" tileheight="16" tilecount="84" columns="6">
- <image source="tilesetUpdated2.png" width="96" height="224"/>
-</tileset>
